8b6e93b6ad9e919316dae71ded8080d9fbca0957
Tharunfitfun <tharunfitfun204@gmail.com>
2025-01-31 07:36:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,102 +1,78 @@
+#(©)t.me/Outlawbots
+
+
+
+
 import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-# Bot Token from @BotFather
+
+
+#Bot token @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-
-# Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", "0"))  # Defaults to 0 to prevent errors
-
-# Your API Hash from my.telegram.org
+#Your API ID from my.telegram.org
+APP_ID = int(os.environ.get("APP_ID", ""))
+#Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "")
-
-# Your Database Channel ID
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002256509918"))
-
-# Owner Info
+#Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002306870616"))
+#OWNER USERNAMA OWNER
 OWNER = os.environ.get("OWNER", "Tharun_stryker")
+#OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "6586630448"))
-
-# Owner Username (without '@')
-OWNER_USER = os.environ.get("OWNER_USER", "").strip()
-
-# Port
-PORT = int(os.environ.get("PORT", "8030"))
-
-# Database Config
+# OWNER USERNAME WITHOUT @ REQUIRED 
+OWNER_USER = os.environ.get("OWNER_USER", " ")
+#Port
+PORT = os.environ.get("PORT", "8030")
+#Database
 DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster8")
+#force sub channel id, if you want enable force sub
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002306870616"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002306307006"))
 
-# Force Subscribe Channel
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002306307006"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002306870616"))
-
-# Number of Bot Workers
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
-# Default Images
 START_PIC = os.environ.get("START_PIC", "https://iili.io/2QFefUb.jpg")
 FORCE_PIC = os.environ.get("FORCE_PIC", "https://iili.io/2QFefUb.jpg")
 
-# Help Text
-HELP_TXT = """<b>ᴛʜɪs ɪs ᴀ ᴍᴏᴠɪᴇ ʙᴏᴛ ғᴏʀ @OutlawBots.
-
-❏ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs:
-├ /start : sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ
-├ /about : ɪɴғᴏ ᴀʙᴏᴜᴛ ʙᴏᴛ
-└ /help : ʜᴏᴡ ᴛᴏ ᴜsᴇ ʙᴏᴛ
-
-ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟs & ɢᴇᴛ ᴍᴏᴠɪᴇs ғᴏʀ ғʀᴇᴇ! 🎥
-ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ <a href="https://t.me/HateXfree">ᯓ ʜᴀᴛᴇ ғʀᴇᴇ ᡣ𐭩</a></b>"""
-
-# About Text
-ABOUT_TXT = f"""<b>
-╭───────────⍟
-├➤ ᴄʀᴇᴀᴛᴏʀ: <a href="https://t.me/InkaLinks">ᴛʜɪs ᴘᴇʀsᴏɴ</a>
-├➤ ʟɪʙʀᴀʀy: <a href="https://github.com/pyrogram">ᴘʏʀᴏɢʀᴀᴍ</a>
-├➤ ʟᴀɴɢᴜᴀɢᴇ: <a href="https://www.python.org">ᴘʏᴛʜᴏɴ 3</a>
-├➤ ᴍʏ ᴜᴘᴅᴀᴛᴇs: <a href="https://t.me/AniHorizon">AniHorizon</a>
-├➤ ᴘᴀɪᴅ ʙᴏᴛ: <a href="https://t.me/ifeelscam">ᯓ ɪɴᴠᴀʟɪᴅ ᡣ𐭩</a>
-├➤ ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href="https://t.me/Tharun_stryker">ᯓ Strykerᡣ𐭩</a>
-╰───────────────⍟</b>"""
-
-# Start Message
-START_MSG = os.environ.get("START_MESSAGE", "<b>Oi {first}!\n\n<blockquote>😏 I'm a file store bot! I keep private files safe in a specified channel, and others can access them through a special link! 📂🔗</blockquote></b>")
-
-# Admin List
+HELP_TXT = "<b>ᴛʜɪs ɪs ᴀɴ ғɪʟᴇ ᴛᴏ ʟɪɴᴋ ʙᴏᴛ ᴡᴏʀᴋ ғᴏʀ @OutlawBots\n\n❏ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs\n├/start : sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ\n├/about : ᴏᴜʀ Iɴғᴏʀᴍᴀᴛɪᴏɴ\n└/help : ʜᴇʟᴘ ʀᴇʟᴀᴛᴇᴅ ʙᴏᴛ\n\n sɪᴍᴘʟʏ ᴄʟɪᴄᴋ ᴏɴ ʟɪɴᴋ ᴀɴᴅ sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ ᴊᴏɪɴ ʙᴏᴛʜ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ ᴛʜᴀᴛs ɪᴛ.....!\n\n ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ <a href=https://t.me/HateXfree>ᯓ ʜᴀᴛᴇ ғʀᴇᴇ ᡣ𐭩</a></b>"
+#Change This Person link 😂 important!!
+ABOUT_TXT = f"""<b><blockquote>╭───────────⍟
+├➤ ᴄʀᴇᴀᴛᴏʀ  : <a href='t.me/InkaLinks'>ᴛʜɪs ᴘᴇʀsᴏɴ</a>
+├➤ ʟɪʙʀᴀʀy : <a href=https://github.com/pyrogram>ᴘʏʀᴏɢʀᴀᴍ</a>
+├➤ ʟᴀɴɢᴜᴀɢᴇ : <a href=https://www.python.org>ᴘʏᴛʜᴏɴ 3</a>
+├➤ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href=https://t.me/outlawbots>ᴏᴜᴛʟᴀᴡ ʙᴏᴛs</a>
+├➤ ᴘᴀɪᴅ ʙᴏᴛ : <a href=https://t.me/ifeelscam>ᯓ ɪɴᴠᴀʟɪᴅ ᡣ𐭩</a>
+├➤ ᴅᴇᴠʟᴏᴘᴇʀ : <a href=https://t.me/HateXfree>ᯓ ʜᴀᴛᴇ ғʀᴇᴇ ᡣ𐭩</a>
+╰───────────────⍟</blockquote></b>"""
+START_MSG = os.environ.get("START_MESSAGE", "<b>ʜᴇʏ !! {first}\n\n <blockquote>ɪ ᴀᴍ ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ᴘʀɪᴠᴀᴛᴇ ғɪʟᴇs ɪɴ sᴘᴇᴄɪғɪᴇᴅ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴏᴛʜᴇʀ ᴜsᴇʀs ᴄᴀɴ ᴀᴄᴄᴇss ɪᴛ ғʀᴏᴍ sᴘᴇᴄɪᴀʟ ʟɪɴᴋ.</blockquote></b>")
 try:
-    ADMINS = [OWNER_ID]
-    admin_list = os.environ.get("ADMINS", "5115691197 6273945163 6103092779 5231212075").split()
-    ADMINS.extend([int(x) for x in admin_list if x.isdigit()])
+    ADMINS=[6376328008]
+    for x in (os.environ.get("ADMINS", "5115691197 6273945163 6103092779 5231212075").split()):
+        ADMINS.append(int(x))
 except ValueError:
-    raise Exception("Your Admins list contains invalid data.")
+        raise Exception("Your Admins list does not contain valid integers.")
 
-# Force Subscribe Message
-FORCE_MSG = os.environ.get(
-    "FORCE_SUB_MESSAGE",
-    "Oi! {mention}\n\n<b>😏 Join our channels first! Then click 'Try Again' to get your file! 🏴‍☠️💰.</b>"
-)
+#Force sub message ,for mention {mention} , for first name {first} , for username {username}
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "ʜᴇʟʟᴏ {mention}\n\n<b><blockquote>ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ᴛʀʏ ᴀɢᴀɪɴ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ʀᴇǫᴜᴇꜱᴛᴇᴅ ꜰɪʟᴇ.</blockquote></b>")
 
-# Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ @AniHorizon</b>")
+#set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>• ʙʏ @OutlawBots</b>")
 
-# Protect Content (Prevent Forwarding)
-PROTECT_CONTENT = os.environ.get("PROTECT_CONTENT", "False").lower() == "true"
+#set True if you want to prevent users from forwarding files from bot
+PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
-# Disable Share Button
-DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", "False").lower() == "true"
+#Set true if you want Disable your Channel Posts Share button
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
-# Bot Stats
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
+USER_REPLY_TEXT = "❌ Dᴏɴ'ᴛ sᴇɴᴅ ᴍᴇ ᴍᴇssᴀɢᴇs ᴅɪʀᴇᴄᴛʟʏ ɪ'ᴍ ᴏɴʟʏ ғɪʟᴇ sʜᴀʀᴇ ʙᴏᴛ!"
 
-# User Reply Text
-USER_REPLY_TEXT = "❌ Oi! 😡 Don't send me messages directly! 😤 I'm just a file share bot, not your personal assistant! 🏴‍☠️💰"
-
-# Add Extra Admins
+ADMINS.append(OWNER_ID)
 ADMINS.append(5191566338)
 
-# Logging
 LOG_FILE_NAME = "filesharingbot.txt"
 
 logging.basicConfig(
@@ -104,12 +80,17 @@ logging.basicConfig(
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
     datefmt='%d-%b-%y %H:%M:%S',
     handlers=[
-        RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
         logging.StreamHandler()
     ]
 )
-
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
+   
